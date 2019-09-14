e35f4bb25072c64a9712dc02481aa455c48a36c1
Nikita Povalishev <povalish.work@gmail.com>
2019-09-14 23:59:11 +0700

    add more examples of messages and users

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -14,6 +14,11 @@ interface IUserMessage extends IMessage {
   user: User;
 }
 
+interface ISystemMessage extends IMessage {
+  user: User;
+  date: Date;
+}
+
 /**
  * User Message model. Defined messages from/to user.
  * Requered properties: user (IUser), body (string), type (MessageType).
@@ -32,14 +37,18 @@ export class UserMessage implements IUserMessage {
 
 /**
  * System Message model. Defined messages about connection/disconnection/etc.
- * Requered properties: body (string), type (MessageType).
+ * Requered properties: body (string), type (MessageType), user (User), date (Date).
  */
-export class SystemMessage implements IMessage {
+export class SystemMessage implements ISystemMessage {
   public type: MessageType;
   public body: string;
+  public user: User;
+  public date: Date;
 
-  constructor(messageData: IMessage) {
+  constructor(messageData: ISystemMessage) {
     this.type = messageData.type;
     this.body = messageData.body;
+    this.user = messageData.user;
+    this.date = new Date();
   }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,28 @@
-export interface IUser {
+export interface IBaseUser {
   login: string;
 }
+
+// tslint:disable-next-line: no-empty-interface
+export interface IUser extends IBaseUser {}
+export interface IAdmin extends IBaseUser {
+  status: string;
+}
+
 
 export class User implements IUser {
   public login: string;
 
   constructor(login: string = 'Unknown') {
     this.login = login;
+  }
+}
+
+export class Admin implements IAdmin {
+  public login: string;
+  public status: string;
+
+  constructor(login: string = 'Unknown', status: string = 'Unknown') {
+    this.login = login;
+    this.status = status;
   }
 }
